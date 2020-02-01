a64b1255c834b355891ec050fa81264eb0f70c13
Brian Rojas <rojasbrian.95@gmail.com>
2020-02-01 00:50:41 -0700

    map is now working with the status card

--- lib/map.dart
+++ lib/map.dart
@@ -37,22 +37,16 @@ class _MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text("hello"),
-      ),
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            }),
+        Positioned(child: StatusCard()),
+      ],
     );
   }
 
