9ce61ec921875dd344541ab41d83bf6c2c4bbb76
Brian Rojas <rojasbrian.95@gmail.com>
2020-02-04 18:20:03 -0700

    getting data from firebase

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coronavirus_app/widgets/table_item.dart';
 import 'package:coronavirus_app/widgets/table_title.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/status_card.dart';
+import 'widgets/status_card_tri.dart';
 
 class Chart extends StatefulWidget {
   @override
@@ -10,45 +11,38 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return TableItem(
+      country: document.documentID,
+      deaths: document['dead'],
+      cases: document['infected'],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: double.infinity,
       child: Column(
         children: <Widget>[
-          StatusCard(),
+          StatusCardTri(),
           TableTitle(),
           Container(
-            color: Colors.transparent,
-            alignment: Alignment.topCenter,
-            height: MediaQuery.of(context).size.height - 283,
-            width: double.infinity,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                  TableItem(),
-                ],
-              ),
-            ),
-          ),
+              color: Colors.transparent,
+              alignment: Alignment.topCenter,
+              height: MediaQuery.of(context).size.height - 283,
+              width: double.infinity,
+              child: StreamBuilder(
+                stream: Firestore.instance.collection('locations').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return const Text('Loading...');
+                  return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) =>
+                        _buildListItem(context, snapshot.data.documents[index]),
+                  );
+                },
+              )),
         ],
       ),
       color: Colors.white,
