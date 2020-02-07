7e7dd57fe336ab43bc99a19cb0036176223b3cb0
BKhush <khushpreetbuttar@gmail.com>
2020-02-07 03:59:49 -0700

    dropshaw and top padding

--- lib/about.dart
+++ lib/about.dart
@@ -19,79 +19,82 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 20,),
-          child: Text(
-            'About',
-            style: TextStyle(
-              color: Theme.of(context).accentColor,
-              fontSize: 45,
+    return Container(
+      padding: EdgeInsets.only(top: 20),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10,),
+            child: Text(
+              'About',
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 45,
+              ),
             ),
           ),
+          centerTitle: false,
+          backgroundColor: Colors.white,
         ),
-        centerTitle: false,
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              ImageSlider(),
-              // ExpAnimated(),
-              ExpAnimated(
-                cardTitle: 'Info\n\n',
-                textHeight: 250,
-                discription: info,
-                color: Color.fromRGBO(245, 220, 216, 1.0),
-              ),
-              ExpAnimated(
-                cardTitle: 'Transmission\n\n',
-                textHeight: 300,
-                discription: transmission,
-                color: Color.fromRGBO(228, 209, 195, 1.0),
-              ),
-              ExpAnimated(
-                cardTitle: 'Symptoms\n\n',
-                textHeight: 250,
-                discription: symptoms,
-                color: Color.fromRGBO(251, 419, 98, 1.0),
-              ),
-              ExpAnimated(
-                cardTitle: 'Prevention\n\n',
-                textHeight: 250,
-                discription: prevention,
-                color: Color.fromRGBO(258, 209, 195, 1.0),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    FlatButton.icon(
-                        onPressed: null,
-                        icon: Icon(Icons.info),
-                        label: Text(
-                          'Info',
-                          style: TextStyle(fontSize: 24),
-                        )),
-                   FlatButton.icon(
-                        onPressed: null,
-                        icon: Icon(Icons.share),
-                        label: Text(
-                          'Share',
-                          style: TextStyle(fontSize: 24),
-                        )),
-                    
-                  ],
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                ImageSlider(),
+                // ExpAnimated(),
+                ExpAnimated(
+                  cardTitle: 'Info\n\n',
+                  textHeight: 250,
+                  discription: info,
+                  color: Color.fromRGBO(245, 220, 216, 1.0),
                 ),
-              ),
-            ],
+                ExpAnimated(
+                  cardTitle: 'Transmission\n\n',
+                  textHeight: 300,
+                  discription: transmission,
+                  color: Color.fromRGBO(228, 209, 195, 1.0),
+                ),
+                ExpAnimated(
+                  cardTitle: 'Symptoms\n\n',
+                  textHeight: 250,
+                  discription: symptoms,
+                  color: Color.fromRGBO(251, 419, 98, 1.0),
+                ),
+                ExpAnimated(
+                  cardTitle: 'Prevention\n\n',
+                  textHeight: 250,
+                  discription: prevention,
+                  color: Color.fromRGBO(258, 209, 195, 1.0),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      FlatButton.icon(
+                          onPressed: null,
+                          icon: Icon(Icons.info),
+                          label: Text(
+                            'Info',
+                            style: TextStyle(fontSize: 24),
+                          )),
+                     FlatButton.icon(
+                          onPressed: null,
+                          icon: Icon(Icons.share),
+                          label: Text(
+                            'Share',
+                            style: TextStyle(fontSize: 24),
+                          )),
+                      
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/exp_animated.dart
+++ lib/widgets/exp_animated.dart
@@ -26,50 +26,68 @@ class _ExpAnimatedState extends State<ExpAnimated> {
   @override
   Widget build(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          if (_height > 100) {
-            _height = 60;
-            _color = widget.color;
-          } else {
-            _height = widget.textHeight;
-            _color = widget.color;
-          }
-        });
-      },
-      child: AnimatedContainer(
-        margin: EdgeInsets.only(top: 20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-          color: widget.color,
-        ),
-        duration: Duration(milliseconds: 1000),
-        curve: Curves.elasticInOut,
-        // curve: Curves.decelerate,
-        height: _height,
-        width: _mediaQueryData.size.width,
-        child: Container(
-          padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 10),
-          child: RichText(
-            text: TextSpan(
-              style: DefaultTextStyle.of(context).style,
-              children: <TextSpan>[
-                TextSpan(
-                  text: widget.cardTitle,
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Theme.of(context).accentColor,
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      decoration: new BoxDecoration(
+        color: Colors.white,
+        // shape: BoxShape.rectangle,
+        borderRadius: new BorderRadius.circular(12.0),
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 4.0, // has the effect of softening the shadow
+            spreadRadius: 2.0, // has the effect of extending the shadow
+            offset: Offset(
+              1.0, // horizontal, move right 10
+              1.0, // vertical, move down 10
+            ),
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_height > 100) {
+              _height = 60;
+              _color = widget.color;
+            } else {
+              _height = widget.textHeight;
+              _color = widget.color;
+            }
+          });
+        },
+        child: AnimatedContainer(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            color: widget.color,
+          ),
+          duration: Duration(milliseconds: 1000),
+          curve: Curves.elasticInOut,
+          // curve: Curves.decelerate,
+          height: _height,
+          width: _mediaQueryData.size.width,
+          child: Container(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 10),
+            child: RichText(
+              text: TextSpan(
+                style: DefaultTextStyle.of(context).style,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: widget.cardTitle,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Theme.of(context).accentColor,
+                    ),
                   ),
-                ),
-                TextSpan(
-                  text: widget.discription,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Theme.of(context).accentColor,
+                  TextSpan(
+                    text: widget.discription,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Theme.of(context).accentColor,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
