e2730b6aab503e8a4a8105d3c79e66d376da6fe2
BKhush <khushpreetbuttar@gmail.com>
2020-02-07 02:41:22 -0700

    animated cards setup done

--- lib/about.dart
+++ lib/about.dart
@@ -5,6 +5,19 @@ import 'widgets/exp_animated.dart';
 // import 'widgets/expandable.dart';
 
 class About extends StatelessWidget {
+  var info =
+      'A novel coronavirus (nCoV) is a new strain of coronavirus that has not been previously identified in humans. The new, or “novel” coronavirus, now called 2019-nCoV, had not previously detected before the outbreak was reported in Wuhan, China in December 2019.';
+
+  var transmission =
+      'Person-to-person spread is thought to occur mainly via respiratory droplets produced when an infected person coughs or sneezes, similar to how influenza and other respiratory pathogens spread. These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs. It’s currently unclear if a person can get 2019-nCoV by touching a surface or object that has the virus on it and then touching their own mouth, nose, or possibly their eyes.';
+
+  var symptoms =
+      'Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death. ';
+
+  var prevention =
+      'Standard recommendations to prevent infection spread include regular hand washing, covering mouth and nose when coughing and sneezing, thoroughly cooking meat and eggs. Avoid close contact with anyone showing symptoms of respiratory illness such as coughing and sneezing.';
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,21 +44,31 @@ class About extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ImageSlider(),
-              ExpAnimated(),
-              // AnimatedCard(),
-              // ExpandableCard(),
-              // ExpandableCard(
-              //   title: "Transmission",
-              //   color: Color.fromRGBO(245, 220, 216, 1.0),
-              // ),
-              // ExpandableCard(
-              //   title: "Symptoms",
-              //   color: Color.fromRGBO(228, 209, 195, 1.0),
-              // ),
-              // ExpandableCard(
-              //   title: "Prevention",
-              //   color: Colors.amber,
-              // ),
+              // ExpAnimated(),
+              ExpAnimated(
+                cardTitle: 'Info\n\n',
+                textHeight: 250,
+                discription: info,
+                color: Color.fromRGBO(245, 220, 216, 1.0),
+              ),
+              ExpAnimated(
+                cardTitle: 'Transmission\n\n',
+                textHeight: 300,
+                discription: transmission,
+                color: Color.fromRGBO(228, 209, 195, 1.0),
+              ),
+              ExpAnimated(
+                cardTitle:'Symptoms\n\n',
+                textHeight: 250,
+                discription: symptoms,
+                color: Color.fromRGBO(251, 419, 98, 1.0),
+              ),
+              ExpAnimated(
+                cardTitle: 'Prevention\n\n',
+                textHeight: 250,
+                discription: prevention,
+                color: Color.fromRGBO(258, 209, 195, 1.0),
+              ),
             ],
           ),
         ),
--- lib/widgets/exp_animated.dart
+++ lib/widgets/exp_animated.dart
@@ -1,77 +1,71 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 
 class ExpAnimated extends StatefulWidget {
+  final String cardTitle;
+  final String discription;
+  final double textHeight;
+  final Color color;
+
+  ExpAnimated({
+    this.cardTitle = 'Title',
+    this.discription = '\n\ndiscription',
+    this.textHeight = 200,
+    this.color = Colors.red,
+  });
+
   @override
   _ExpAnimatedState createState() => _ExpAnimatedState();
 }
 
 class _ExpAnimatedState extends State<ExpAnimated> {
   MediaQueryData _mediaQueryData;
-  Color _color = Color.fromRGBO(245, 220, 216, 1.0);
+  Color _color = Colors.red;
   double _height = 100;
-
-  var info =
-      'A novel coronavirus (nCoV) is a new strain of coronavirus that has not been previously identified in humans. The new, or “novel” coronavirus, now called 2019-nCoV, had not previously detected before the outbreak was reported in Wuhan, China in December 2019.';
-
-  var transmission =
-      'Person-to-person spread is thought to occur mainly via respiratory droplets produced when an infected person coughs or sneezes, similar to how influenza and other respiratory pathogens spread. These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs. It’s currently unclear if a person can get 2019-nCoV by touching a surface or object that has the virus on it and then touching their own mouth, nose, or possibly their eyes.';
-
-  var symptoms = 'Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death.';
-
-  var prevention = 'Standard recommendations to prevent infection spread include regular hand washing, covering mouth and nose when coughing and sneezing, thoroughly cooking meat and eggs. Avoid close contact with anyone showing symptoms of respiratory illness such as coughing and sneezing.';
-
-  void _animate() {
-    setState(() {
-      if (_height > 100) {
-        _height = 100;
-        _color = Color.fromRGBO(245, 220, 216, 1.0);
-      } else {
-        _height = 250;
-        _color = Color.fromRGBO(245, 220, 216, 1.0);
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
     return GestureDetector(
-      onTap: _animate,
+      onTap: () {
+        setState(() {
+          if (_height > 100) {
+            _height = 100;
+            _color = widget.color;
+          } else {
+            _height = widget.textHeight;
+            _color = widget.color;
+          }
+        });
+      },
       child: AnimatedContainer(
         margin: EdgeInsets.only(top: 20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(12)),
-          color: _color,
+          color: widget.color,
         ),
         duration: Duration(milliseconds: 1000),
         curve: Curves.elasticInOut,
-        // color: _color,
         height: _height,
         width: _mediaQueryData.size.width,
         child: Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 10),
           child: RichText(
             text: TextSpan(
               style: DefaultTextStyle.of(context).style,
               children: <TextSpan>[
-                TextSpan(text: '\n\n'),
                 TextSpan(
-                  text: 'Info \n\n',
+                  text: widget.cardTitle,
                   style: TextStyle(
                     fontSize: 24,
                     color: Theme.of(context).accentColor,
-                    // fontWeight: FontWeight.bold,
                   ),
                 ),
                 TextSpan(
-                  text:info,
+                  text: widget.discription,
                   style: TextStyle(
                     fontSize: 14.0,
                     color: Theme.of(context).accentColor,
-                    // fontWeight: FontWeight.bold,
                   ),
                 ),
               ],
