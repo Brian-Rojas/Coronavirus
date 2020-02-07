a09246dcb459a565bd5946868b396f18417e1b79
BKhush <khushpreetbuttar@gmail.com>
2020-02-07 04:11:50 -0700

    fixed padding and dropshadow

--- lib/news.dart
+++ lib/news.dart
@@ -15,36 +15,39 @@ class News extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Text(
-            'News',
-            style: TextStyle(
-              color: Theme.of(context).accentColor,
-              fontSize: 45,
+    return Container(
+      padding: EdgeInsets.only(top:20),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10,),
+            child: Text(
+              'News',
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 45,
+              ),
             ),
           ),
+          centerTitle: false,
+          backgroundColor: Colors.white,
         ),
-        centerTitle: false,
-        backgroundColor: Colors.white,
-      ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('news')
-            .orderBy("time", descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const Text('Loading...');
-          return ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) =>
-                _buildListItem(context, snapshot.data.documents[index]),
-          );
-        },
+        body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('news')
+              .orderBy("time", descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          },
+        ),
       ),
     );
   }
