fca50821f4c70f74836ca83ffaa8913840c79be4
Brian Rojas <rojasbrian.95@gmail.com>
2020-02-18 01:45:05 -0700

    changed marker image

--- lib/models/markers.dart
+++ lib/models/markers.dart
@@ -23,7 +23,7 @@ class Markers with ChangeNotifier {
       ImageConfiguration(
         devicePixelRatio: 2.5,
       ),
-      'assets/images/marker4.png',
+      'assets/images/marker5.png',
     );
   }
 
